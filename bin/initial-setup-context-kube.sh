b5a33f2c4a8f75659ff9c7cfd4d453a3b6648b9b
Jonas Cavalcanti <jonascavalcantineto@gmail.com>
2018-06-29 17:01:58 -0300

    Update start

--- bin/initial-setup-context-kube.sh
+++ bin/initial-setup-context-kube.sh
@@ -8,28 +8,32 @@ mkdir -p ${DIR_CERTS_USERS} ${DIR_CERTS_MODULES}
 echo "GENERATE CERTS ON $DIR_CERTS"
 
 #SETUP KUBECTL ADMIN
-openssl genrsa -out ${DIR_CERTS_USERS}/${USER}-key.pem 2048
-openssl req -new -key ${DIR_CERTS_USERS}/${USER}-key.pem -out ${DIR_CERTS_USERS}/${USER}.csr -subj "/CN=${USER}"
-openssl x509 -req -in ${DIR_CERTS_USERS}/${USER}.csr -CA ${DIR_CERTS_API}/${CA_CERT_PEM} -CAkey ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} -CAcreateserial -out ${DIR_CERTS_USERS}/${USER}.pem -days 7200
+if [ ! -f ${DIR_CERTS_USERS}/${USER}-key.pem ]; then
+    echo "CERTIFICATES ${DIR_CERTS_USERS} not found!"
+    openssl genrsa -out ${DIR_CERTS_USERS}/${USER}-key.pem 2048
+    openssl req -new -key ${DIR_CERTS_USERS}/${USER}-key.pem -out ${DIR_CERTS_USERS}/${USER}.csr -subj "/CN=${USER}"
+    openssl x509 -req -in ${DIR_CERTS_USERS}/${USER}.csr -CA ${DIR_CERTS_API}/${CA_CERT_PEM} -CAkey ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} -CAcreateserial -out ${DIR_CERTS_USERS}/${USER}.pem -days 7200
 
 
-kubectl config set-cluster $CLUSTER_NAME --certificate-authority=${DIR_CERTS_API}/${CA_CERT_PEM} --embed-certs=true --server=https://${APISERVER_IP}:6443 
-kubectl config set-credentials ${USER} --client-certificate=${DIR_CERTS_USERS}/${ADMIN_CERT_PEM} --client-key=${DIR_CERTS_USERS}/${ADMIN_KEY_PEM} --embed-certs=true --token=${TOKEN}
-kubectl config set-context $CONTEXT_NAME --cluster=$CLUSTER_NAME --user=${USER}
-kubectl config use-context $CONTEXT_NAME
+    kubectl config set-cluster $CLUSTER_NAME --certificate-authority=${DIR_CERTS_API}/${CA_CERT_PEM} --embed-certs=true --server=https://${APISERVER_IP}:6443 
+    kubectl config set-credentials ${USER} --client-certificate=${DIR_CERTS_USERS}/${ADMIN_CERT_PEM} --client-key=${DIR_CERTS_USERS}/${ADMIN_KEY_PEM} --embed-certs=true --token=${TOKEN}
+    kubectl config set-context $CONTEXT_NAME --cluster=$CLUSTER_NAME --user=${USER}
+    kubectl config use-context $CONTEXT_NAME
 
-for user in kube-controller-manager kube-scheduler kubelet kube-proxy
-do
-    openssl genrsa -out ${DIR_CERTS_MODULES}/${user}/${user}-key.pem 2048
-    openssl req -new -key ${DIR_CERTS_MODULES}/${user}/${user}-key.pem -out ${DIR_CERTS_MODULES}/${user}/${user}.csr -subj "/CN=${user}"
-    openssl x509 -req -in ${DIR_CERTS_MODULES}/${user}/${user}.csr -CA ${DIR_CERTS_API}/${CA_CERT_PEM} -CAkey ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} -CAcreateserial -out ${DIR_CERTS_MODULES}/${user}/${user}.pem -days 7200
+    for modulo in kube-controller-manager kube-scheduler kubelet kube-proxy kube-dns kubernetes-dashboard
+    do
+        mkdir -p ${DIR_CERTS_MODULES}/${modulo}
+        openssl genrsa -out ${DIR_CERTS_MODULES}/${modulo}/${modulo}-key.pem 2048
+        openssl req -new -key ${DIR_CERTS_MODULES}/${modulo}/${modulo}-key.pem -out ${DIR_CERTS_MODULES}/${modulo}/${modulo}.csr -subj "/CN=${modulo}"
+        openssl x509 -req -in ${DIR_CERTS_MODULES}/${modulo}/${modulo}.csr -CA ${DIR_CERTS_API}/${CA_CERT_PEM} -CAkey ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} -CAcreateserial -out ${DIR_CERTS_MODULES}/${modulo}/${modulo}.pem -days 7200
 
-    kubectl config set-cluster $CLUSTER_NAME --certificate-authority=${DIR_CERTS_API}/${CA_CERT_PEM} --embed-certs=true --server=https://${APISERVER_IP}:6443  --kubeconfig=${DIR_CERTS_MODULES}/${user}/kubeconfig
-    kubectl config set-credentials ${user} --client-certificate=${DIR_CERTS_MODULES}/${user}/${user}.pem --client-key=${DIR_CERTS_MODULES}/${user}/${user}-key.pem --embed-certs=true --token=$TOKEN --kubeconfig=${DIR_CERTS_MODULES}/${user}/kubeconfig
-    kubectl config set-context $CONTEXT_NAME --cluster=$CLUSTER_NAME --user=${user} --kubeconfig=${DIR_CERTS_MODULES}/${user}/kubeconfig
-    kubectl config use-context $CONTEXT_NAME --kubeconfig=${DIR_CERTS_MODULES}/${user}/kubeconfig
-
-done
+        kubectl config set-cluster $CLUSTER_NAME --certificate-authority=${DIR_CERTS_API}/${CA_CERT_PEM} --embed-certs=true --server=https://${APISERVER_IP}:6443  --kubeconfig=${DIR_CERTS_MODULES}/${modulo}/kubeconfig
+        kubectl config set-credentials ${modulo} --client-certificate=${DIR_CERTS_MODULES}/${modulo}/${modulo}.pem --client-key=${DIR_CERTS_MODULES}/${modulo}/${modulo}-key.pem --embed-certs=true --token=$TOKEN --kubeconfig=${DIR_CERTS_MODULES}/${modulo}/kubeconfig
+        kubectl config set-context $CONTEXT_NAME --cluster=$CLUSTER_NAME --user=${modulo} --kubeconfig=${DIR_CERTS_MODULES}/${modulo}/kubeconfig
+        kubectl config use-context $CONTEXT_NAME --kubeconfig=${DIR_CERTS_MODULES}/${modulo}/kubeconfig
+    done
+else 
+    echo "CERTIFICATES ${DIR_CERTS_USERS} found!"
+fi
 
 chown -R kube:kube ${PATH_BASE_KUBERNETES}/
-
--- bin/start_apiserver.sh
+++ bin/start_apiserver.sh
@@ -5,14 +5,19 @@ cp /openssl.cnf  ${DIR_CERTS_API}/openssl.cnf
 
 
 #GENERATE CERTIFICATES 
+if [ ! -f ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} ]; then
+    echo "CERTIFICATES ${DIR_CERTS_API} not found!"
 
-openssl genrsa -out ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} 2048
-openssl req -x509 -new -nodes -key ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} -days 10000 -out ${DIR_CERTS_API}/${CA_CERT_PEM} -subj "/CN=${CLUSTER_NAME}"
-openssl genrsa -out ${DIR_CERTS_API}/${API_KEY_PEM} 2048
-openssl req -new -key ${DIR_CERTS_API}/${API_KEY_PEM} -out ${DIR_CERTS_API}/${API_CERT_CSR} -subj "/CN=kube-apiserver" -config ${DIR_CERTS_API}/openssl.cnf
-openssl x509 -req -in ${DIR_CERTS_API}/${API_CERT_CSR} -CA ${DIR_CERTS_API}/${CA_CERT_PEM} -CAkey ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} -CAcreateserial -out ${DIR_CERTS_API}/${API_CERT_PEM} -days 7200 -extensions v3_req -extfile ${DIR_CERTS_API}/openssl.cnf
-cp -a ${DIR_CERTS_API}/${API_CERT_PEM} ${DIR_CERTS_API}/${API_CERT_CRT}
-cp -a ${DIR_CERTS_API}/${API_KEY_PEM} ${DIR_CERTS_API}/${API_KEY}
+    openssl genrsa -out ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} 2048
+    openssl req -x509 -new -nodes -key ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} -days 10000 -out ${DIR_CERTS_API}/${CA_CERT_PEM} -subj "/CN=${CLUSTER_NAME}"
+    openssl genrsa -out ${DIR_CERTS_API}/${API_KEY_PEM} 2048
+    openssl req -new -key ${DIR_CERTS_API}/${API_KEY_PEM} -out ${DIR_CERTS_API}/${API_CERT_CSR} -subj "/CN=kube-apiserver" -config ${DIR_CERTS_API}/openssl.cnf
+    openssl x509 -req -in ${DIR_CERTS_API}/${API_CERT_CSR} -CA ${DIR_CERTS_API}/${CA_CERT_PEM} -CAkey ${DIR_CERTS_API}/${CA_CERT_PEM_KEY} -CAcreateserial -out ${DIR_CERTS_API}/${API_CERT_PEM} -days 7200 -extensions v3_req -extfile ${DIR_CERTS_API}/openssl.cnf
+    cp -a ${DIR_CERTS_API}/${API_CERT_PEM} ${DIR_CERTS_API}/${API_CERT_CRT}
+    cp -a ${DIR_CERTS_API}/${API_KEY_PEM} ${DIR_CERTS_API}/${API_KEY}
+else 
+    echo "CERTIFICATES ${DIR_CERTS_API} found!"
+fi
 
 chown -R kube:kube ${PATH_BASE_KUBERNETES}/
 
@@ -30,4 +35,7 @@ chown -R kube:kube ${PATH_BASE_KUBERNETES}/
     --allow-privileged=true \
     --audit-log-path=/var/log/apiserver/ \
     --audit-log-format=legacy \
+    --admission-control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ServiceAccount,ResourceQuota \
     --service-node-port-range=20-32767
+
+ #--authorization-mode=RBAC \
